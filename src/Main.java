9a95b167f2775a6bc704496d29b1848c35f11ef8
Gustavo Guimaraes <guuuhh666@gmail.com>
2026-06-24 13:57:51 -0300

    add file sorting and basic analyzer functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import model.ArquivoInfo;
 import service.AnalisadorDisco;
+import util.FormatadorTamanho;
 
 import java.util.Scanner;
 import java.util.List;
@@ -8,16 +9,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         AnalisadorDisco analisador = new AnalisadorDisco();
+        FormatadorTamanho formatador = new FormatadorTamanho();
 
         System.out.print("Digite o caminho da pasta/arquivo: ");
         String caminho = sc.nextLine();
 
         List<ArquivoInfo> arquivos = analisador.analisar(caminho);
+        arquivos.sort((a, b ) -> Long.compare(b.getTamanho(), a.getTamanho()));
 
-        System.out.println("Arquivos encontrados: " +  arquivos.size());
+        System.out.println("\n=== ARQUIVOS ORDENADOS POR TAMANHO ===\n");
 
         for(ArquivoInfo a : arquivos){
-            System.out.println(a.getNome() + " - " + a.getTamanho() + " bytes");
+            System.out.println(a.getNome() + " - " + formatador.formatar(a.getTamanho()));
         }
     }
 }
---
+++ src/util/FormatadorTamanho.java
@@ -0,0 +1,27 @@
+package util;
+
+public class FormatadorTamanho {
+    public String formatar(Long bytes) {
+
+        if (bytes < 1024) {
+            return bytes + " B";
+        }
+
+        double kb = bytes / 1024;
+
+        if (kb < 1024) {
+        return String.format("%.2f KB", kb);
+    }
+
+    double mb = kb / 1024.0;
+
+        if (mb < 1024) {
+        return String.format("%.2f MB", mb);
+
+        }
+
+        double gb = mb  / 1024.0;
+
+        return String.format("%.2f GB", gb);
+    }
+}
