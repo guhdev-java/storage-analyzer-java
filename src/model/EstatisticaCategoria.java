47e07b04337e43a2779b2bccf4f53e0a2003c5d9
Gustavo Guimaraes <guuuhh666@gmail.com>
2026-06-25 20:44:49 -0300

    add file sorting and basic analyzer functionality

---
+++ src/model/EstatisticaCategoria.java
@@ -0,0 +1,20 @@
+package model;
+
+public class EstatisticaCategoria {
+
+    private int quantidadeArquivos;
+    private long tamanhoTotal;
+
+    public void adicionarArquivo(long tamanho){
+        quantidadeArquivos++;
+        tamanhoTotal += tamanho;
+    }
+
+    public int getQuantidadeArquivos(){
+        return quantidadeArquivos;
+    }
+
+    public long getTamanhoTotal(){
+        return tamanhoTotal;
+    }
+}
--- src/service/AnalisadorDisco.java
+++ src/service/AnalisadorDisco.java
@@ -1,19 +1,23 @@
 package service;
 
 import model.ArquivoInfo;
+import model.EstatisticaCategoria;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class AnalisadorDisco {
 
     public List<ArquivoInfo> analisar (String caminho) {
         List<ArquivoInfo> arquivos = new ArrayList<>();
+        Map<String, EstatisticaCategoria> estatisticas = new HashMap<>();
 
         Path pasta = Paths.get(caminho);
 
@@ -27,6 +31,11 @@ public class AnalisadorDisco {
                             String caminhoCompleto = arquivo.toString();
                             long tamanho = Files.size(arquivo);
 
+                            String categoria = descobrirCategoria(nome);
+                            estatisticas.putIfAbsent(categoria, new EstatisticaCategoria());
+                            estatisticas.get(categoria)
+                                    .adicionarArquivo(tamanho);
+
                             ArquivoInfo info =  new ArquivoInfo(nome, caminhoCompleto, tamanho);
 
                             arquivos.add(info);
@@ -38,6 +47,44 @@ public class AnalisadorDisco {
         } catch (IOException e) {
             System.out.println("Erro ao ler pasta: " + e.getMessage());
         }
+
+        for (Map.Entry<String, EstatisticaCategoria> entry : estatisticas.entrySet()){
+
+            String categoria = entry.getKey();
+            EstatisticaCategoria info = entry.getValue();
+
+            System.out.println("Categoria: " + categoria);
+            System.out.println("Quantidade: " + info.getQuantidadeArquivos());
+            System.out.println("Tamanho total: " + info.getTamanhoTotal() + " bytes");
+        }
+
         return arquivos;
+    }
+    private String descobrirCategoria(String nomeArquivo) {
+        String nome =  nomeArquivo.toLowerCase();
+
+        if (nome.endsWith(".jpg") ||
+            nome.endsWith(".jpeg") ||
+            nome.endsWith(".png") ||
+            nome.endsWith(".gif")) {
+
+            return "Imagens";
+        }
+
+        if (nome.endsWith(".mp4") ||
+            nome.endsWith(".avi") ||
+            nome.endsWith(".mkv")) {
+
+            return "Videos";
+        }
+
+        if (nome.endsWith(".pdf") ||
+            nome.endsWith(".doc") ||
+            nome.endsWith(".docx") ||
+            nome.endsWith(".txt")) {
+
+            return "Documentos";
+        }
+        return "Outros";
     }
 }
